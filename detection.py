83b4bb3050bd5782ac93fdd9e0e08fcb448d3b25
phil.lipuchen <phil.lipuchen@gmail.com>
2021-12-05 14:23:27 -0500

    change from HOG to CNN

--- detection.py
+++ detection.py
@@ -9,6 +9,11 @@ def detect_faces(img):
         dets: a list of dlib rectangles
     """
 
-    detector = dlib.get_frontal_face_detector()
-    dets = detector(img)
-    return dets
+    cnn_face_detector = dlib.cnn_face_detection_model_v1('mmod_human_face_detector.dat')
+    dets = cnn_face_detector(img, 1)
+
+    #transfer from cnn_rectangles to dlib rectangles
+    rects = dlib.rectangles()
+    rects.extend([d.rect for d in dets])
+
+    return rects
