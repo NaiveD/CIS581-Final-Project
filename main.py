5573b6a5673396f92b92d2566b876fc25c1cabc4
Zhaikeyan <zhaiky@shanghaitech.edu.cn>
2021-12-05 16:17:21 -0500

    not work

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from morphing import ImageMorphingTriangulation, face_swap
 from blending import blending
 from optical_flow import optical_flow
 
-video_source = './2.mp4'
-video_target = './1.mp4'
+video_source = './2.mp4' # MrRobot
+video_target = './1.mp4' # FrankUnderwood
 
 frame_rate = 5
 needDetection = True
@@ -19,14 +19,22 @@ def main():
     target_num_frame = int(cap_target.get(cv2.CAP_PROP_FRAME_COUNT))
     num_frame = min(source_num_frame, target_num_frame)
 
+    # For writing the video
+    # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    # fps = cap_target.get(cv2.CAP_PROP_FPS)
+    # size = (int(cap_target.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap_target.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    # writer = cv2.VideoWriter('Results/output.avi', fourcc, fps, size)
+
     count_frame = 0
     while True:
         is_source, source_frame = cap_source.read()
         is_target, target_frame = cap_target.read()
         if is_source and is_target and count_frame < num_frame:
             count_frame += 1
-            target_pos_frame = cap_target.get(cv2.CAP_PROP_POS_FRAMES)
+            target_pos_frame = cap_target.get(cv2.CAP_PROP_POS_FRAMES) # index of the current target frame
+            print("current target frame: %d" % target_pos_frame)
             if (target_pos_frame - 1) % frame_rate == 0 or needDetection:
+                print("Performing feature detection")
                 # STEP 1: Do detection
                 det_source = detect_faces(source_frame)
                 det_target = detect_faces(target_frame)
@@ -42,11 +50,16 @@ def main():
                 # STEP 4: Seamless Blending
                 output = blending(result, source_frame, convexhull)
                 cv2.imshow("Result", output)
+                # writer.write(output) # write the output to the video
 
                 prev_target_frame = target_frame
                 needDetection = False
             else:
-                output = optical_flow(output, feature_target, target_frame, prev_target_frame)
+                # Perform optical flow
+                print("Performing optical flow")
+                output, feature_target = optical_flow(output, feature_target, target_frame, prev_target_frame)
+                cv2.imshow("Result", output)
+                # writer.write(output)
                 prev_target_frame = target_frame
 
         key = cv2.waitKey(1)
@@ -56,6 +69,7 @@ def main():
     cv2.destroyAllWindows()
     cap_source.release()
     cap_target.release()
+    # writer.release()
 
 
 if __name__ == '__main__':
--- optical_flow.py
+++ optical_flow.py
@@ -1,12 +1,80 @@
+import numpy as np
+import cv2
+from skimage.transform import SimilarityTransform
+from morphing import ImageMorphingTriangulation
+    
+lk_params = dict(winSize  = (15,15),
+                maxLevel = 2,
+                criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-def optical_flow(prev_output, target_features, target_frame, prev_target_frame): # target_pos_frame):
+def optical_flow(prev_output, prev_target_features, target_frame, prev_target_frame):
     """
         input:
-            prev_output: output from last frame
-            target_features：feature points of target frame
-            target_frame: target frame image
+            prev_output: last output frame image
+            prev_target_features: feature points in the last target frame
+            target_frame: current target frame image
             prev_target_frame: last target frame image
+            
         output:
-            output
-        """
-    pass
+            output: current output frame image
+            feature_target: feature points in the current target frame      
+    """
+    
+    feature_points = np.asarray(prev_target_features).astype(np.float32)[:, :, None]
+    feature_points = np.transpose(feature_points, (0, 2, 1))
+
+    prev_gray = cv2.cvtColor(prev_target_frame, cv2.COLOR_BGR2GRAY)
+    curr_gray = cv2.cvtColor(target_frame, cv2.COLOR_BGR2GRAY)
+    
+    new_features, st, err = cv2.calcOpticalFlowPyrLK(prev_gray, curr_gray, feature_points, None, **lk_params) # Gunnar Farneback's algorithm
+    
+    good_new = new_features[st==1]
+    good_old = feature_points[st==1]
+    
+    new_output = np.copy(prev_output)
+    
+    transform = SimilarityTransform()
+    
+    if transform.estimate(good_old, good_new):
+        new_output = transform_image(good_old, good_new, prev_output, target_frame)
+    
+    good_new_features = []
+    for each in good_new.tolist():
+        s = []
+        for i in range(len(each)):
+            s.append(each[i])
+        good_new_features.append(tuple(s))
+
+    return new_output, good_new_features
+
+def transform_image(good_old, good_new, prev_output, target_frame):
+    warped_img1 = np.copy(target_frame)
+
+    hullIndex = cv2.convexHull(np.array(good_new).astype(np.int32), returnPoints=False)
+    
+    hull1 = [good_old[int(hullIndex[i])] for i in range(0, len(hullIndex))]
+    hull2 = [good_new[int(hullIndex[i])] for i in range(0, len(hullIndex))]
+
+    # hull1, hull2 = convex_hull(points1.tolist(), points2.tolist())
+    hull1 = np.array(hull1).astype(np.float32)
+    hull2 = np.array(hull2).astype(np.float32)
+
+    if (len(hull1) == 0 or len(hull2) == 0):
+        return target_frame
+        
+    hull2 = np.asarray(hull2)
+    hull2[:, 0] = np.clip(hull2[:, 0], 0, target_frame.shape[1] - 1)
+    hull2[:, 1] = np.clip(hull2[:, 1], 0, target_frame.shape[0] - 1)
+    hull2_list = []
+    for each in hull2.astype(np.float32).tolist():
+        s = []
+        for i in range(len(each)):
+            s.append(each[i])
+        hull2_list.append(tuple(s))
+    hull2 = hull2_list
+
+    new_image = ImageMorphingTriangulation(prev_output, target_frame, good_old, good_new)
+    return new_image
+    
+    
+    
